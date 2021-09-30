c5a79b4519bfc8ca819c3d6669e8c2b683ed53ed
monik43 <monimons666@gmail.com>
2021-09-30 10:31:09 +0200

    test historial tickets

--- models/helpdesk.py
+++ models/helpdesk.py
@@ -12,15 +12,27 @@ class helpdesk_stage(models.Model):
 
 class helpdesk_ticket(models.Model):
     _inherit = "helpdesk.ticket"
-    
+
+    def _get_historial_tickets(self):
+        for ticket in self:
+            for t in self.env['helpdesk.ticket'].search(['|', ('x_sn', '=', ticket.x_sn), ('x_lot_id', '=', ticket.x_lot_id)]):
+                print(f"{t.name} -- {t.id}, {t.stage_id.name} #")
+
     @api.onchange('stage_id')
     def onchange_stage_id_eq_sla_id(self):
         for ticket in self:
+            # asignacion politica ans correcta
             if not ticket.stage_id.sla_id and self.env['helpdesk.sla'].search([('name', '=', ticket.stage_id.name)]):
-                ticket.sla_id = self.env['helpdesk.sla'].search([('name', '=', ticket.stage_id.name)])
+                ticket.sla_id = self.env['helpdesk.sla'].search(
+                    [('name', '=', ticket.stage_id.name)])
             elif ticket.stage_id.sla_id:
                 ticket.sla_id = ticket.stage_id.sla_id
-
+            # asignacion usuario x defecto
             if ticket.stage_id.def_assign and ticket.user_id != ticket.stage_id.def_assign:
-                print(f"default: {ticket.stage_id.def_assign} _ user_id: {ticket.user_id}")
+                print(
+                    f"default: {ticket.stage_id.def_assign} _ user_id: {ticket.user_id}")
                 ticket.user_id = ticket.stage_id.def_assign
+
+    historial_tickets = fields.One2many(
+        'helpdesk.ticket', 'Tickets anteriores', compute="_get_historial_tickets")
+    #historial_tickets = fields.One2many('helpdesk.ticket')
