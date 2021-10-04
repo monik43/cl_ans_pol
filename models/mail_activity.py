ea24b42e543909bfee353d150316434b25aeaabe
monik43 <monimons666@gmail.com>
2021-10-04 10:56:09 +0200

    test mail activity

--- models/mail_activity.py
+++ models/mail_activity.py
@@ -2,11 +2,27 @@
 
 from collections import defaultdict
 from datetime import date, datetime, time, timedelta
+import pytz
 from odoo import api, fields, models, _
 
 
 class mail_activity(models.Model):
     _inherit = "mail.activity"
+
+    @api.onchange('activity_type_id')
+    def _onchange_activity_type_id(self):
+        if self.activity_type_id:
+            self.summary = self.activity_type_id.summary
+            print(self.summary)
+            tz = self.user_id.sudo().tz
+            print(tz)
+            if tz:
+                today_utc = pytz.UTC.localize(datetime.utcnow())
+                today = today_utc.astimezone(pytz.timezone(tz))
+            else:
+                today = datetime.now()
+            print(self.activity_type_id.days)
+            self.date_deadline = (today + timedelta(days=self.activity_type_id.days))
 
     @api.model
     def create(self, values):
