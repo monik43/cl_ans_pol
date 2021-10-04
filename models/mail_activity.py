ce9677cd32d6bf3d7fc28a7d31bc3296bd3e8d59
monik43 <monimons666@gmail.com>
2021-10-04 11:06:34 +0200

    test mail activity

--- models/mail_activity.py
+++ models/mail_activity.py
@@ -9,7 +9,7 @@ from odoo import api, fields, models, _
 class mail_activity(models.Model):
     _inherit = "mail.activity"
 
-    @api.onchange('activity_type_id')
+    @api.onchange('activity_type_id, user_id')
     def _onchange_activity_type_id(self):
         if self.activity_type_id:
             self.summary = self.activity_type_id.summary
@@ -20,17 +20,9 @@ class mail_activity(models.Model):
             else:
                 today = datetime.now()
 
-            print((today + timedelta(days=self.activity_type_id.days)).weekday(), "/"*25)
-            self.date_deadline = (today + timedelta(days=self.activity_type_id.days))
-
-    @api.model
-    def create(self, values):
-        # already compute default values to be sure those are computed using the current user
-        values_w_defaults = self.default_get(self._fields.keys())
-        values_w_defaults.update(values)
-        dt = datetime.strptime(values_w_defaults['date_deadline'], '%Y-%m-%d')
+        #dt = datetime.strptime(values_w_defaults['date_deadline'], '%Y-%m-%d')
         laborables = tuple()
-        for dia in self.env['res.users'].browse(values_w_defaults['user_id']).resource_calendar_id.attendance_ids:
+        """for dia in self.env['res.users'].browse(values_w_defaults['user_id']).resource_calendar_id.attendance_ids:
             laborables += (int(dia.dayofweek),)
         print(dt, laborables)
 
@@ -43,14 +35,8 @@ class mail_activity(models.Model):
                     break
         else:
             print("dt in laborables")
-        print("end")
-        # continue as sudo because activities are somewhat protected
-        activity = super(mail_activity, self.sudo()).create(values_w_defaults)
-        activity_user = activity.sudo(self.env.user)
-        activity_user._check_access('create')
-        self.env[activity_user.res_model].browse(activity_user.res_id).message_subscribe(partner_ids=[activity_user.user_id.partner_id.id])
-        if activity.date_deadline <= fields.Date.today():
-            self.env['bus.bus'].sendone(
-                (self._cr.dbname, 'res.partner', activity.user_id.partner_id.id),
-                {'type': 'activity_updated', 'activity_created': True})
-        return activity_user
+        print("end")"""
+
+        print((today + timedelta(days=self.activity_type_id.days)).weekday(), "/"*25)
+        self.date_deadline = (
+            today + timedelta(days=self.activity_type_id.days))
