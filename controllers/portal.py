ccc51c4b58d709a0cf52b6e587dc9fba639be442
monik43 <monimons666@gmail.com>
2021-10-21 12:42:37 +0200

    test controller

--- controllers/portal.py
+++ controllers/portal.py
@@ -12,30 +12,14 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
 class CustomCustomerPortal(CustomerPortal):
-    def _prepare_portal_layout_values(self):
-        # get customer sales rep
-        sales_user = False
-        partner = request.env.user.partner_id
+    @route(["/my", "/my/home"], type="http", auth="user", website=True)
+    def home(self, **kw):
+        res = super(CustomCustomerPortal, self).home()
         user_is_company = True
-
-        if partner.user_id and not partner.user_id._is_public():
-            sales_user = partner.user_id
-
         if (
             request.env["res.users"].browse(request.uid).partner_id.company_type
             == "person"
         ):
             user_is_company = False
-        print(f" Test --- {user_is_company}", "/" * 50)
-        return {
-            "sales_user": sales_user,
-            "page_name": "home",
-            "archive_groups": [],
-            "user_is_company": user_is_company,
-        }
-
-    @route(["/my", "/my/home","/m"], type="http", auth="user", website=True)
-    def home(self, **kw):
-        values = self._prepare_portal_layout_values()
-        print(f" Test --- {values['user_is_company']}", "/" * 50)
-        return request.render("portal.portal_my_home", values)
+        print(f" _prepare_portal_layout_values --- {user_is_company}", "/" * 50)
+        return res
