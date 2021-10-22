7e74a9af7a36a4c05657e7ffc4f91c27acdb16ce
monik43 <monimons666@gmail.com>
2021-10-22 09:32:17 +0200

    customerportal controller

--- controllers/portal.py
+++ controllers/portal.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 
-from odoo import http
-from odoo.http import Controller, request, route
+from odoo import http, _
+from odoo.exceptions import AccessError
+from odoo.http import request, route
+from odoo.tools import consteq
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
 class CustomCustomerPortal(CustomerPortal):
+    def _prepare_portal_layout_values(self):
+        # get customer sales rep
+        sales_user = False
+        partner = request.env.user.partner_id
+        user_is_company = True
+
+        if partner.user_id and not partner.user_id._is_public():
+            sales_user = partner.user_id
+
+        if request.env["res.users"].browse(request.uid).partner_id.company_type == "person":
+            user_is_company = False
+
+        print(f" _prepare_portal_layout_values ---- {user_is_company}", "/" * 50)
+        return {
+            "user_is_company": user_is_company,
+            "sales_user": sales_user,
+            "page_name": "home",
+            "archive_groups": [],
+        }
+
     @route(["/my", "/my/home"], type="http", auth="user", website=True)
     def home(self, **kw):
-        res = super(CustomCustomerPortal, self).home(**kw)
-        user_is_company = True
-        if (
-            request.env["res.users"].browse(request.uid).partner_id.company_type
-            == "person"
-        ):
-            user_is_company = False
-        print(f" _prepare_portal_layout_values ---- {user_is_company}", "/" * 50)
-        return res
+        values = self._prepare_portal_layout_values()
+        print(values)
+        return request.render("portal.portal_my_home", values)
