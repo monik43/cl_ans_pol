2753dedcaa63a8794642283a6d44545abc2130c2
monik43 <monimons666@gmail.com>
2021-10-27 11:52:11 +0200

    cleanup

--- models/helpdesk.py
+++ models/helpdesk.py
@@ -22,6 +22,9 @@ class helpdesk_ticket(models.Model):
             ):
                 if t.id != ticket.id and (t.active in (True, False)):
                     ticket.update({"historial_tickets": [(4, t.id)]})
+            for tic in ticket.historial_tickets:
+                if tic.x_lot_id.id != ticket.x_lot_id.id:
+                    ticket.update({"historial_tickets": [(3, tic.id)]})
 
     historial_tickets = fields.One2many(
         "helpdesk.ticket",
@@ -30,6 +33,13 @@ class helpdesk_ticket(models.Model):
         context={"active_test": False},
     )
     # historial_tickets = fields.One2many('helpdesk.ticket')
+
+    @api.onchange("x_lot_id")
+    def onchange_x_lot_id_unlink(self):
+        for ticket in self:
+            for tic in ticket.historial_tickets:
+                if tic.x_lot_id.id != ticket.x_lot_id.id:
+                    ticket.update({"historial_tickets": [(3, tic.id)]})
 
     @api.onchange("stage_id")
     def onchange_stage_id_eq_sla_id(self):
@@ -51,37 +61,8 @@ class helpdesk_ticket(models.Model):
                 ticket.user_id = ticket.stage_id.def_assign
             ticket_create_date = fields.Datetime.from_string(ticket.create_date)
             working_calendar = self.env.user.company_id.resource_calendar_id
-            ##al cambiar de estado, se cambia la fecha de deadline
-            print(f"""
-                    ticket.sla_id.time_days -> {ticket.sla_id.time_days} \n
-                    ticket_create_date -> {ticket_create_date}\n
-                    working_calendar -> {working_calendar}\n
-
-                    """)
-            """if ticket.sla_id.time_days > 0:
-                deadline = working_calendar.plan_days(
-                        ticket.sla_id.time_days+1,
-                        ticket_create_date,
-                        compute_leaves=True)
-                    # We should also depend on ticket creation time, otherwise for 1 day SLA for example all tickets
-                    # created on monday will have the deadline as tuesday 8:00
-                deadline = deadline.replace(hour=ticket_create_date.hour, minute=ticket_create_date.minute, second=ticket_create_date.second, microsecond=ticket_create_date.microsecond)
-            else:
-                deadline = ticket_create_date
-                # We should execute the function plan_hours in any case because
-                # if i create a ticket for 1 day sla configuration and tomorrow at the same time i don't work,
-                # deadline falls on the time that i don't work which is ticket creation time and is not correct
-                ticket.deadline = working_calendar.plan_hours(
-                    ticket.sla_id.time_hours,
-                    deadline,
-                    compute_leaves=True)"""
-
-
-    # team_id -> siempre el mismo
-    # priority -> no relevante
-    # ticket_type_id -> no usado
-    # create_date
-    @api.depends('stage_id','create_date')
+            
+    @api.depends("stage_id", "create_date")
     def _compute_sla(self):
         if not self.user_has_groups("helpdesk.group_use_sla"):
             return
@@ -106,21 +87,26 @@ class helpdesk_ticket(models.Model):
                 working_calendar = self.env.user.company_id.resource_calendar_id
                 if ticket.sla_id.time_days > 0:
                     deadline = working_calendar.plan_days(
-                        ticket.sla_id.time_days+1,
+                        ticket.sla_id.time_days + 1,
                         ticket_create_date,
-                        compute_leaves=True)
+                        compute_leaves=True,
+                    )
                     # We should also depend on ticket creation time, otherwise for 1 day SLA for example all tickets
                     # created on monday will have the deadline as tuesday 8:00
-                    deadline = deadline.replace(hour=ticket_create_date.hour, minute=ticket_create_date.minute, second=ticket_create_date.second, microsecond=ticket_create_date.microsecond)
+                    deadline = deadline.replace(
+                        hour=ticket_create_date.hour,
+                        minute=ticket_create_date.minute,
+                        second=ticket_create_date.second,
+                        microsecond=ticket_create_date.microsecond,
+                    )
                 else:
                     deadline = ticket_create_date
                 # We should execute the function plan_hours in any case because
                 # if i create a ticket for 1 day sla configuration and tomorrow at the same time i don't work,
                 # deadline falls on the time that i don't work which is ticket creation time and is not correct
                 ticket.deadline = working_calendar.plan_hours(
-                    ticket.sla_id.time_hours,
-                    deadline,
-                    compute_leaves=True)
+                    ticket.sla_id.time_hours, deadline, compute_leaves=True
+                )
 
     @api.multi
     def open_ticket(self):
