993c6a1f180557fa0845ca8c391ae65bdee07492
monik43 <monimons666@gmail.com>
2021-11-04 12:26:53 +0100

    prueba lot existente

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -8,6 +8,6 @@ class stock_production_lot(models.Model):
 
     def _compute_lot_assigned(self):
         for rec in self:
-            if self.env['mrp.repair.line'].search([('lot_id','=',rec.lot_id.id)]):
+            if self.env['mrp.repair.line'].search([('lot_id','=',rec.lot_id.id),('lot_id','!=', '9999')]):
                 print("El nº de lote ya está asignado en otra reparación. ", "/"*50)
                 rec.lot_assigned = True
