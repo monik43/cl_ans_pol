e8e5774a11ec795b8e694997e6d8503d604967b5
monik43 <monimons666@gmail.com>
2021-11-05 12:37:26 +0100

    test rapidez

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -4,18 +4,11 @@ from odoo import api, fields, models, _
 class stock_production_lot(models.Model):
     _inherit = "stock.production.lot"
 
-    #lot_assigned = fields.Boolean("Lote ya asignado", default=False, compute="_compute_lot_assigned", store=True)
+    lot_assigned = fields.Boolean("Lote ya asignado", default=False, compute="_compute_in_repair_lines", store=True)
     in_repair_lines = fields.One2many('mrp.repair.line', 'lot_id', "Lineas de operaciones")
 
     def _compute_in_repair_lines(self):
         for rec in self:
             for line in self.env['mrp.repair.line'].browse([('lot_id','=',rec.id)]):
                 rec.update({"in_repair_lines": [(4, line.id)]})
-
-    """@api.depends("product_id")
-    def _compute_lot_assigned(self):
-        for rec in self:
-            print(rec.product_id, " ", rec.name, " ", "//"*25)
-            if rec.product_id and self.env['mrp.repair.line'].search([('lot_id.id','=',rec.id)]):
-                #rec.lot_assigned = True
-                print(rec.lot_assigned, rec.product_id, "/"*50)"""
+                rec.lot_assigned = True
