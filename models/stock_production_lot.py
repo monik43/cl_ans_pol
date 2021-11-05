c99922c06a9c02584f9581a24962171e5b6a934a
monik43 <monimons666@gmail.com>
2021-11-05 11:01:57 +0100

    test rapidez

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, _
 class stock_production_lot(models.Model):
     _inherit = "stock.production.lot"
 
-    lot_assigned = fields.Boolean("Lote ya asignado", default=False, compute="_compute_lot_assigned", store=True)
+    lot_assigned = fields.Boolean("Lote ya asignado", default=False, compute="_compute_lot_assigned")
 
     def _compute_lot_assigned(self):
         for rec in self:
