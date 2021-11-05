23a1660b9487938393cb8594de854ca441ecfe6b
monik43 <monimons666@gmail.com>
2021-11-05 13:14:15 +0100

    onchange location_id test

---
+++ models/mrp_repair.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class mrp_repair(models.Model):
+    _inherit = "mrp.repair"
+
+    @api.onchange('location_id')
+    def onchange_location_id(self):
+        self.location_dest_id = self.location_id.id
+
+        for line in self.operations:
+            line.location_id = self.location_id
