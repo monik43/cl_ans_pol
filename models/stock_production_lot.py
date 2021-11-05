f2a157aa481e16b5fdfbabd23423840631c501a5
monik43 <monimons666@gmail.com>
2021-11-05 11:39:29 +0100

    test rapidez

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -6,7 +6,7 @@ class stock_production_lot(models.Model):
 
     lot_assigned = fields.Boolean("Lote ya asignado", default=False, compute="_compute_lot_assigned", store=True)
 
-    @api.depends("id", "product_id")
+    @api.depends("product_id")
     def _compute_lot_assigned(self):
         for rec in self:
             if rec.product_id and rec.name != '9999' and self.env['mrp.repair.line'].search([('lot_id.id','=',rec.id)]):
