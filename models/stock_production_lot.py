fcce802af1c55d9322d41ac0e0bf778639d154d8
monik43 <monimons666@gmail.com>
2021-11-05 12:50:09 +0100

    repair lines como metodo

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -4,11 +4,10 @@ from odoo import api, fields, models, _
 class stock_production_lot(models.Model):
     _inherit = "stock.production.lot"
 
-    lot_assigned = fields.Boolean("Lote ya asignado", default=False, compute="_compute_in_repair_lines")
+    
     in_repair_lines = fields.One2many('mrp.repair.line', 'lot_id', "Lineas de operaciones")
 
     def _compute_in_repair_lines(self):
         for rec in self:
             for line in self.env['mrp.repair.line'].browse([('lot_id','=',rec.id)]):
                 rec.update({"in_repair_lines": [(4, line.id)]})
-                rec.lot_assigned = True
