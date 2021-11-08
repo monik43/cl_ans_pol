7dfd412b256ae9262959ede1366d8fb81d204bb8
monik43 <monimons666@gmail.com>
2021-11-08 09:40:33 +0100

    test line operations

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -5,9 +5,8 @@ class mrp_repair(models.Model):
     _inherit = 'mrp.repair'
 
     @api.onchange('location_id')
-    def onchange_location_id_tst(self):
+    def onchange_location_id(self):
         self.location_dest_id = self.location_id.id
-        print("()()()"*50)
+
         for line in self.operations:
-            print(line.location_id, " ", "\\"*25)
             line.location_id = self.location_id
