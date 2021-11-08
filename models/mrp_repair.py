ea529639f3f837d2afe82defcebbc7233c07f9f3
monik43 <monimons666@gmail.com>
2021-11-08 09:38:21 +0100

    test line operations

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -2,12 +2,12 @@
 from odoo import api, fields, models, _
 
 class mrp_repair(models.Model):
-    _inherit = "mrp.repair"
+    _inherit = 'mrp.repair'
 
     @api.onchange('location_id')
     def onchange_location_id_tst(self):
         self.location_dest_id = self.location_id.id
-
+        print("()()()"*50)
         for line in self.operations:
             print(line.location_id, " ", "\\"*25)
             line.location_id = self.location_id
